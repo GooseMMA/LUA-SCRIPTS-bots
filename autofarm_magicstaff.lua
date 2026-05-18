e02cb4a9401db8708395ef6325895f931e0f157b
goozling <104625675+GooseMMA@users.noreply.github.com>
2026-05-18 21:16:16 +0200

    Update autofarm_magicstaff.lua

--- autofarm_magicstaff.lua
+++ autofarm_magicstaff.lua
@@ -1,20 +1,15 @@
-@echo off
-echo ============================
-echo Forward 8080 -> 127.0.0.1:8080
-echo ============================
+============================
+============================
 
-netsh interface portproxy add v4tov4 ^
-listenaddress=0.0.0.0 listenport=8080 ^
-connectaddress=127.0.0.1 connectport=8080
+ОК.
 
-netsh advfirewall firewall add rule ^
-name="Open 8080" dir=in action=allow protocol=TCP localport=8080
 
-echo.
-echo DONE
-echo.
-ipconfig | findstr /i "IPv4"
-echo.
-echo Теперь открой на телефоне:
-echo http://IP_КОМПА:8080
-pause
+DONE
+
+   IPv4-адрес. . . . . . . . . . . . : 26.53.113.232
+   IPv4-адрес. . . . . . . . . . . . : 172.16.0.2
+   IPv4-адрес. . . . . . . . . . . . : 192.168.1.42
+
+╨в╨╡╨┐╨╡╤А╤М ╨╛╤В╨║╤А╨╛╨╣ ╨╜╨░ ╤В╨╡╨╗╨╡╤Д╨╛╨╜╨╡:
+http://IP_╨Ъ╨Ю╨Ь╨Я╨Р:8080
+Для продолжения нажмите любую клавишу . . .
